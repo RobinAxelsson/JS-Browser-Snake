8100a7380117fe73cfe37d5d692102dff086bf31
RobinAxelsson <axelsson.robin@outlook.com>
2021-08-24 09:46:50 +0200

    about to implement gameboard in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+//TODO menu with onclick and logging key choices
+//TODO add dynamic style, for easy make, plus name rendering on snake
+//TODO testing and breaking up new code
+//TODO keeping score
+
 import { DEFAULT_GAME } from "./modules/gamesettings.js";
 import { GameManager } from "./modules/GameManager.js";
 
